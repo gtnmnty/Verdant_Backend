e3f9746bbc94106c90983e3c982b87bd854dae16
Monty <bgwhiskey64@gmail.com>
2026-06-13 22:04:15 +0800

    initialized backend

---
+++ src/main/java/com/gilded/salon_ecomm/SalonEcommApplication.java
@@ -0,0 +1,12 @@
+package com.gilded.salon_ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonEcommApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SalonEcommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gilded/salon_ecomm/SalonEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilded.salon_ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
